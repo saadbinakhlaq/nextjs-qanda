b86e2d3d92755bd0ec00d0c9fc79671bfa072326
Saad Akhlaq <saad.akhlaq@smartly.io>
2024-09-12 18:20:35 +0200

    Publish

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/nextjs-qanda",
+    output: "export",
+    reactStrictMode: true,
+};
 
 export default nextConfig;
